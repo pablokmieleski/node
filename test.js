e24468e7585a7626f922b412e6e9a1f01e0251fe
pablokmieleski <pablo.kmieleski@gmail.com>
2020-02-09 13:44:17 +0000

    node fun

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 const moment = require('moment');
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));  
 console.log(moment().format("MMM Do YY"));  
+console.log(moment().startOf('hour').fromNow());  
